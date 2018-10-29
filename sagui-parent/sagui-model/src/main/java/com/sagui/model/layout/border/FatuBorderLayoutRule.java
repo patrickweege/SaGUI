c517b9d4c723b246129c76c4ecbcce001f2531ca
patrickweege <patrickweege@gmail.com>
2018-10-29 19:42:25 -0200

    Add Constant for BorderLayoutRule

--- sagui-parent/sagui-model/src/main/java/com/sagui/model/layout/border/FatuBorderLayoutRule.java
+++ sagui-parent/sagui-model/src/main/java/com/sagui/model/layout/border/FatuBorderLayoutRule.java
@@ -5,10 +5,19 @@ import com.sagui.model.layout.IFatuLayoutRule;
 public class FatuBorderLayoutRule implements IFatuLayoutRule<FatuBorderLayout> {
 	
 	public static final FatuBorderLayoutRule NORTH = new FatuBorderLayoutRule(FatuRegion.NORTH);
+	public static final FatuBorderLayoutRule NORTH_SPLIT = new FatuBorderLayoutRule(FatuRegion.NORTH, true);
+
 	public static final FatuBorderLayoutRule SOUTH = new FatuBorderLayoutRule(FatuRegion.SOUTH);
+	public static final FatuBorderLayoutRule SOUTH_SPLIT = new FatuBorderLayoutRule(FatuRegion.SOUTH, true);
+
 	public static final FatuBorderLayoutRule WEST = new FatuBorderLayoutRule(FatuRegion.WEST);
+	public static final FatuBorderLayoutRule WEST_SPLIT = new FatuBorderLayoutRule(FatuRegion.WEST, true);
+
 	public static final FatuBorderLayoutRule EAST = new FatuBorderLayoutRule(FatuRegion.EAST);
+	public static final FatuBorderLayoutRule EAST_SPLIT = new FatuBorderLayoutRule(FatuRegion.EAST, true);
+
 	public static final FatuBorderLayoutRule CENTER = new FatuBorderLayoutRule(FatuRegion.CENTER);
+	public static final FatuBorderLayoutRule CENTER_SPLIT = new FatuBorderLayoutRule(FatuRegion.CENTER, true);
 	
 	private final FatuRegion region;
 	private final boolean split;
